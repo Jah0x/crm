9d5632df7d649bdd165b7924ce87d8c90b3e41ff
Jah0x <petra12231@gmail.com>
2025-06-04 13:47:54 +0300

    fix setup

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import reactPlugin from "eslint-plugin-react";
 import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
@@ -10,9 +11,13 @@ export default [
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        project: "./tsconfig.json",
+        project: "./tsconfig.eslint.json",
         sourceType: "module",
         ecmaVersion: "latest",
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
       },
     },
     plugins: {
